85d1df11293c99ae2a26d9d96fdcc55837b615b4
simorinka <lebrmn.37@gmail.com>
2023-05-29 14:33:11 +1000

    first commit

---
+++ Infa_2/Infa_2.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int reverse(int number) {
+	int count = 0;
+	while (number != 0) {
+		count = count * 10 + number % 10;
+		number /= 10;
+	}
+	return count;
+}
+
+int main() {
+
+}
