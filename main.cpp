5f57e88437ca56364aa2960453d3c0a593331de0
Егор <@DESKTOP-L47E61Q>
2023-05-29 14:41:56 +1000

    Something

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int reverse(int number){
+
+}
+
+int main() {
+    int number;
+    int count = reverse(number);
+    if(count==number){
+        cout<<"Yes"<<endl;
+    }
+    else
+        cout<<"No"<<endl;
+
+    return 0;
+}
